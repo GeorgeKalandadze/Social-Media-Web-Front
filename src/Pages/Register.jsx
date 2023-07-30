673cffcbd51e22728b19b9284ff23b642464880a
George Kalandadze <kalandadzeg777@gmail.com>
2023-07-31 00:21:25 +0400

    feat:add registration functionality and its errors

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GuestLayout from '../Layouts/GuestLayout'
 import LeftSide from '../Components/Auth/LeftSide'
 import SocialImg from '../assets/social-people-image-2.jpg'
 import RightSide from '../Components/Auth/RightSide'
 import Input from '../Components/Auth/Input'
+import { postRequest } from '../Axios/axiosClient'
 
 
 const Register = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirmation, setPasswordConfirmation] = useState('')
+    const [errors, setErrors] = useState({})
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        try {
+            const data = {name:name, email: email, password: password, password_confirmation:passwordConfirmation };
+            const response = await postRequest("/register", data);
+          } catch (error) {
+            if (error.response && error.response.data) {
+                const { errors: validationErrors } = error.response.data;
+                setErrors(validationErrors);
+              } else {
+                console.error("Error posting data:", error);
+              }
+          }
+      };
+
   return (
     <GuestLayout className={"flex-row-reverse"}>
         <LeftSide 
@@ -18,23 +40,34 @@ const Register = () => {
             link="/"
         />
         <RightSide className={"justify-center"}>
-            <form className='flex flex-col gap-[30px]'>
+            <form className='flex flex-col gap-[30px]' onSubmit={handleRegister}>
                 <h1 className='text-[#555] font-bold text-[30px]'>Sign Up</h1>
                 <Input
                     placeholder="Enter Name"
                     type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    error={errors.name}
                 />
                 <Input
                     placeholder="Enter Email"
                     type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    error={errors.email}
                 />
                 <Input
                     placeholder="Enter Password"
                     type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    error={errors.password}
                 />
                 <Input
                     placeholder="Enter Password confirmation"
                     type="password"
+                    value={passwordConfirmation}
+                    onChange={(e) => setPasswordConfirmation(e.target.value)}
                 />
                 <button className="w-1/2 py-2 px-4 border-none bg-[#938ceb] text-white  cursor-pointer">
                     Click Me
