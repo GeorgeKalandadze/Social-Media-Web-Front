ef7995e7e9be1602ebbbfd5cc6464287cc3d7c3e
George Kalandadze <kalandadzeg777@gmail.com>
2023-08-08 23:20:30 +0400

    feat:make form for post create

--- src/Components/Post/PostForm.jsx
+++ src/Components/Post/PostForm.jsx
@@ -1,14 +1,20 @@
 import { Box, Modal } from '@mui/material'
 import React from 'react'
+import UploadImages from './UploadImages';
+import InputGroup from './InputGroup';
+import { useDispatch } from 'react-redux';
+import { closeModal } from '../../Redux/postModalSlice';
+
 
 const PostForm = ({open, close}) => {
+    const dispatch = useDispatch();
     const style = {
     position: 'fixed',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: '95%',
-    maxWidth: 800,
+    maxWidth: 600,
     bgcolor: 'background.paper',
     boxShadow: 14,
     borderRadius: 1,
@@ -27,7 +33,33 @@ const PostForm = ({open, close}) => {
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
-            <Box sx={style} component="form" noValidate autoComplete="off"></Box>
+            <Box sx={style} component="form" noValidate autoComplete="off">
+                <h1 className="text-center text-[26px] font-bold text-gray-500 opacity-40 mb-4">Create New Post</h1>
+                <UploadImages/>
+                <InputGroup
+                    label="Title"
+                    placeholder="Enter Title for post"
+                />
+                <InputGroup
+                    label="Content"
+                    placeholder="Enter Content for post"
+                />
+                <div className="flex items-center justify-between">
+                    <button
+                        type="button"
+                        className="mt-6 bg-[#423dce] text-white px-6 py-3 font-medium rounded"
+                    >
+                        Create Post
+                    </button>
+                    <button
+                        type="button"
+                        className="mt-6 bg-red-700 text-white px-6 py-3 font-medium rounded"
+                        onClick={() => dispatch(closeModal())}
+                    >
+                        Close
+                    </button>
+                </div>
+            </Box>
         </Modal>
     </div>
   )
---
+++ src/Components/Post/UploadImages.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef, useState } from 'react'
+import uploadImg from '../../assets/cloud-upload-regular-240.png';
+import './style.css'
+
+const UploadImages = () => {
+    const wrapperRef = useRef(null);
+    const [fileList, setFileList] = useState([]);
+    const onDragEnter = () => wrapperRef.current.classList.add('dragover');
+    const onDragLeave = () => wrapperRef.current.classList.remove('dragover');
+    const onDrop = () => wrapperRef.current.classList.remove('dragover');
+    const [productsData, setProductsData] = useState({
+      name: '',
+      slug: '',
+      quote: '',
+      price: null,
+      quantity: null,
+      published: 0,
+      description: '',
+      discount: null,
+    });
+  
+    const onFileDrop = (e) => {
+      const newFiles = Array.from(e.target.files);
+      if (newFiles.length > 0) {
+        const updatedList = [...fileList, ...newFiles];
+        setFileList(updatedList);
+        setProductsData((prevData) => ({
+          ...prevData,
+          images: updatedList,
+        }));
+      }
+    };
+  
+    useEffect(() => {
+      setProductsData((prevData) => ({
+        ...prevData,
+        images: fileList,
+      }));
+    }, [fileList]);
+
+    const customClassname = `grid ${
+        fileList.length === 1
+          ? "grid-cols-1"
+          : fileList.length === 2
+          ? "grid-cols-2 gap-2"
+          : fileList.length === 3
+          ? "grid-cols-2 grid-rows-2 gap-2"
+          : ""
+      }`;
+
+    return (
+      <div className='border-[1.7px] border-gray-200 rounded p-2 mb-4'>
+        {fileList.length === 0 ? (
+          <div
+            ref={wrapperRef}
+            className=" relative w-full h-[300px] border-2 border-dashed border-gray-300 rounded-md flex items-center justify-center bg-gray-100 hover:opacity-60 mt-2"
+            onDragEnter={onDragEnter}
+            onDragLeave={onDragLeave}
+            onDrop={onDrop}
+          >
+            <div className="drop-file-input__label text-center text-gray-600 font-semibold p-2 flex flex-col items-center">
+              <img src={uploadImg} alt="" className="w-24 rounded-full" />
+              <p>Drag & Drop your Images here</p>
+            </div>
+            <input
+              type="file"
+              value=""
+              onChange={onFileDrop}
+              className="opacity-0 absolute top-0 left-0 w-full h-full cursor-pointer"
+              multiple
+            />
+          </div>
+        ) : (
+          <div className={customClassname}>
+            {fileList.map((file, index) => (
+              <img
+                key={index}
+                src={URL.createObjectURL(file)}
+                className="w-full h-[300px] rounded post-image"
+              />
+            ))}
+          </div>
+        )}
+     
+      </div>
+    );
+  };
+  
+  export default UploadImages;
+  
