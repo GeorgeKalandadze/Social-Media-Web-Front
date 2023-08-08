4b9b56006d539d809b48e83cb0243dd7efad0a7c
George Kalandadze <kalandadzeg777@gmail.com>
2023-08-09 02:03:25 +0400

    feat:make cascading dropdown for categories and subcategories

--- src/Components/Post/PostForm.jsx
+++ src/Components/Post/PostForm.jsx
@@ -1,15 +1,17 @@
 import { Box, Modal } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UploadImages from './UploadImages';
 import InputGroup from './InputGroup';
 import { useDispatch, useSelector } from 'react-redux';
 import { closeModal } from '../../Redux/postModalSlice';
 import { updateData } from '../../Redux/postDataSlice';
+import  { getRequest } from '../../Axios/axiosClient';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import {  FormControl} from '@mui/material';
 
-
-const PostForm = ({open, close}) => {
-    const dispatch = useDispatch();
-    const style = {
+const style = {
     position: 'fixed',
     top: '50%',
     left: '50%',
@@ -28,15 +30,38 @@ const PostForm = ({open, close}) => {
 };
 
 
-const data = useSelector((state) => state.postData);
+const PostForm = ({open, close}) => {
+    const dispatch = useDispatch();
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('');
+    const [selectedSubcategory, setSelectedSubcategory] = useState('');
+    const [filteredSubcategories, setFilteredSubcategories] = useState([]);
 
-const handleInputChange = (event) => {
-  const { name, value } = event.target;
-  const newData = { [name]: value };
-  dispatch(updateData(newData));
-};
+    const data = useSelector((state) => state.postData);
 
-console.log(data)
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        const newData = { [name]: value };
+        dispatch(updateData(newData));
+    };
+
+    useEffect(() => {
+        getRequest('/categories')
+        .then((res) => {
+            setCategories(res.data);
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    },[])
+
+    const handleCategoryChange = (e) => {
+        const selectedCategoryId = e.target.value;
+        const selectedCategory = categories.find((category) => category.id === selectedCategoryId);
+        setSelectedCategory(selectedCategoryId);
+        setFilteredSubcategories(selectedCategory?.sub_categories || []);
+        setSelectedSubcategory('');
+    };
 
   return (
     <div>
@@ -49,6 +74,48 @@ console.log(data)
             <Box sx={style} component="form" noValidate autoComplete="off">
                 <h1 className="text-center text-[26px] font-bold text-gray-500 opacity-40 mb-4">Create New Post</h1>
                 <UploadImages/>
+                <div className="mt-4 flex flex-col gap-2">
+                        <label className=" text-[18px] text-gray-400 mb-2">Category</label>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                label="Category"
+                                value={selectedCategory}
+                                onChange={handleCategoryChange}
+                            >
+                                {categories.map((category) => (
+                                    <MenuItem key={category.id} value={category.id}>
+                                        {category.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </div>
+                    <div className="mt-4 flex flex-col gap-2 mb-4">
+                        <label className=" text-[18px] text-gray-400 mb-2">Subcategory</label>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Subcategory</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                label="Subcategory"
+                                value={selectedSubcategory}
+                                name="sub_category_id"
+                                onChange={(e) => {
+                                    setSelectedSubcategory(e.target.value);
+                                    handleInputChange(e);
+                                }}
+                            >
+                                {filteredSubcategories.map((subcategory) => (
+                                    <MenuItem key={subcategory.id} value={subcategory.id}>
+                                        {subcategory.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </div>
                 <InputGroup
                     label="Title"
                     placeholder="Enter Title for post"
