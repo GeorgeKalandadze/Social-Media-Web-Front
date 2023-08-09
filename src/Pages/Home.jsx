682dcd574bd8fa60d79a58eda9be96a67ff0bc3d
George Kalandadze <kalandadzeg777@gmail.com>
2023-08-09 20:42:18 +0400

    feat:make post delete functionality

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,7 +10,6 @@ import { Menu, MenuItem } from "@mui/material";
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const [anchorEl, setAnchorEl] = useState([]);
-  const [openCreateProduct, setOpenCreateProduct] = useState(false);
   const open = Boolean(anchorEl);
 
   useEffect(() => {
@@ -22,6 +21,22 @@ const Home = () => {
       console.log(err)
     })
   },[])
+
+  const deletePost= (postId) => {
+    axiosClient
+      .delete(`/post/${postId}`)
+      .then((response) => {
+        const deletedIndex = posts.findIndex(
+          (post) => post.id === postId
+        );
+        const updatedPosts = [...posts];
+        updatedPosts.splice(deletedIndex, 1);
+        setPosts(updatedPosts);
+      })
+      .catch((error) => {
+        console.error("Error deleting product:", error);
+      });
+  };
 
   const handleClick = (event, index) => {
     const newAnchorElArray = [...anchorEl];
@@ -40,19 +55,15 @@ const Home = () => {
       <div className="flex flex-col gap-6">
         {posts.map((post, index) => (
           <>
-            <Post 
-              props={post} 
-              openModal={handleClick}
-              isOpen={open}
-            />
+            <Post props={post} openModal={handleClick} isOpen={open} />
             <Menu
               MenuListProps={{
                 "aria-labelledby": `long-button-${post.id}`,
               }}
               id={`menu-${post.id}`}
-              anchorEl={anchorEl[index]}
-              open={Boolean(anchorEl[index])}
-              onClose={() => handleClose(index)}
+              anchorEl={anchorEl[post.id]}
+              open={Boolean(anchorEl[post.id])}
+              onClose={() => handleClose(post.id)}
               PaperProps={{
                 style: {
                   maxHeight: 48 * 4.5,
@@ -63,8 +74,8 @@ const Home = () => {
                 <EditIcon sx={{ color: "#818cf8", marginRight: "10px" }} />
                 Edit
               </MenuItem>
-              <MenuItem>
-                <DeleteIcon sx={{ color: "#818cf8", marginRight: "10px" }} />{" "}
+              <MenuItem onClick={() => deletePost(post.id)}>
+                <DeleteIcon sx={{ color: "#818cf8", marginRight: "10px" }} />
                 Delete
               </MenuItem>
             </Menu>
